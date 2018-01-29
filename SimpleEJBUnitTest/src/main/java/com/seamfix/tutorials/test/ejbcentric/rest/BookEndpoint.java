1ae30d4de305f044064f60c6df9be3123a3b20d5
uonuoha@seamfix.com <uonuoha@seamfix.com>
2018-01-29 17:45:46 +0100

    Added restful Test

--- SimpleEJBUnitTest/src/main/java/com/seamfix/tutorials/test/ejbcentric/rest/BookEndpoint.java
+++ SimpleEJBUnitTest/src/main/java/com/seamfix/tutorials/test/ejbcentric/rest/BookEndpoint.java
@@ -1,70 +1,42 @@
 package com.seamfix.tutorials.test.ejbcentric.rest;
 
 import com.seamfix.tutorials.test.ejbcentric.ejb.BookEJB;
+import com.seamfix.tutorials.test.ejbcentric.ejb.dto.BookRequest;
 import com.seamfix.tutorials.test.ejbcentric.model.Book;
-import javax.inject.Inject;
+import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
-import java.util.List;
+import javax.ws.rs.core.MediaType;
 
-
-@Path("/books")
+@Path("/book")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class BookEndpoint {
 
-  
-    @Inject
+    @EJB
     private BookEJB bookService;
 
     @POST
-    @Consumes("application/xml")
-    public Response create(Book entity) {
-        bookService.create(entity);
-        return Response.created(UriBuilder.fromResource(BookEndpoint.class).path(String.valueOf(entity.getId())).build()).build();
-    }
-
-    @DELETE
-    @Path("/{id}")
-    public Response deleteById(@PathParam("id") Long id) {
-        Book deletableEntity = bookService.findById(id);
-        if (deletableEntity == null) {
-            return Response.status(Status.NOT_FOUND).build();
+    @Path("/create")
+    public Response create(BookRequest req) {
+        Book newBook = new Book(req.getIsbn(), req.getTitle(), req.getPrice(), req.getNbOfPages());
+        try {
+            bookService.create(newBook);
+        } catch (Exception ex) {
+            System.out.println("Message=== " + ex.getMessage());
+            ex.printStackTrace();
         }
-        bookService.delete(deletableEntity);
-        return Response.noContent().build();
+        return Response.ok(String.valueOf(newBook.getId())).build();
     }
 
-    @GET
-    @Path("/{id}")
-    @Produces("application/xml")
-    public Response findById(@PathParam("id") Long id) {
-        Book entity = bookService.findByIdWithRelations(id);
-        if (entity == null) {
-            return Response.status(Status.NOT_FOUND).build();
-        }
-        return Response.ok(entity).build();
+    @POST
+    @Path("/data")
+    public Response createe(BookRequest req) {
+        return Response.created(UriBuilder.fromPath("/book/data/").path(String.valueOf(req.getIsbn())).build()).build();
     }
 
-    @GET
-    @Produces("application/xml")
-    public List<Book> findAll() {
-        final List<Book> results = bookService.findAll();
-        return results;
-    }
-
-    @PUT
-    @Path("/{id}")
-    @Consumes("application/xml")
-    public Response update(Book entity) {
-        bookService.update(entity);
-        return Response.noContent().build();
-    }
 }
--- SimpleEJBUnitTest/src/test/java/com/seamfix/tutorials/test/services/BillingServiceTest.java
+++ SimpleEJBUnitTest/src/test/java/com/seamfix/tutorials/test/services/BillingServiceTest.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.seamfix.tutorials.test.ejbcentric;
+package com.seamfix.tutorials.test.services;
 
-import com.seamfix.tutorial.test.jee.EjbWithMockitoRunner;
+import com.seamfix.test.tutorial.jee.EjbWithMockitoRunner;
 import com.seamfix.tutorials.test.ejbcentric.ejb.BookEJB;
 import static com.seamfix.tutorials.test.ejbcentric.ejb.util.TConstant.PRICE_PER_PAGE;
 import com.seamfix.tutorials.test.ejbcentric.model.Book;
@@ -23,26 +23,26 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(EjbWithMockitoRunner.class)
 public class BillingServiceTest {
-    
+
     private static final long BOOK_ID = 15;
-    
+
     @EJB
     BillingService billingService;
-    
+
     @Mock
     private BookEJB bookDao;
-    
+
     @Test
     public void testTotalBookPricePerPage() {
-        Book book = new Book(BOOK_ID, "IBNS-344-42555", "The Papa", 10.5f, 50);
+        Book book = new Book("IBNS-344-42555", "The Papa", 10.5f, 50);
+        book.setId(BOOK_ID);
         when(bookDao.findById(BOOK_ID)).thenReturn(book);
         double expectedTotalPrice = book.getNbOfPages() * PRICE_PER_PAGE;
         //call testing method
         double actualTotalPrice = billingService.getBookPricePerPage(book);
         Assert.assertNotNull(actualTotalPrice);
-        // Assert.assertEquals(expectedTotalPrice, actualTotalPrice);
         Assert.assertEquals(String.valueOf(expectedTotalPrice), String.valueOf(actualTotalPrice));
-        
+
     }
-    
+
 }
